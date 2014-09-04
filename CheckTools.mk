d4d7957487cc2e49dcbe130abc3e15e38e772980
Alexander Light <scialexlight@gmail.com>
2014-09-04 11:19:31 -0400

    make us check rust version

---
+++ CheckTools.mk
@@ -0,0 +1,21 @@
+
+RUST_VERSION := $(shell $(RUST) --version | cut -d - -f1)
+RUST_NAME := $(firstword $(RUST_VERSION))
+RUST_NUMBER := $(subst ., ,$(lastword $(RUST_VERSION)))
+RUST_MAJOR := $(word 1,$(RUST_NUMBER))
+RUST_MINOR := $(word 2,$(RUST_NUMBER))
+RUST_PATCH := $(word 3,$(RUST_NUMBER))
+
+ifneq ($(RUST_NAME),rustc)
+$(error Found $(RUST_NAME) but expected rustc)
+else
+ifneq ($(RUST_MAJOR),0)
+$(error Found version $(RUST_MAJOR) expected 0)
+else
+ifneq ($(RUST_MINOR),12)
+$(error Found $(RUST) version $(RUST_MINOR) expected 12)
+else
+$(info Using $(RUST) version '$(RUST_VERSION)')
+endif
+endif
+endif
--- Global.mk
+++ Global.mk
@@ -3,6 +3,7 @@ LD        := ld
 AR        := ar
 PYTHON    := python
 CSCOPE    := cscope
+RUST      := rustc
 
 CFLAGS    += -fno-builtin -nostdinc -ansi -g3 -fno-stack-protector -m32 -march=i686 -fsigned-char -Iinclude
 CFLAGS    += -Wall -Wredundant-decls -Wundef -Wpointer-arith -Wfloat-equal -Wnested-externs -Wvla -Winline -Wextra -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable
@@ -11,6 +12,7 @@ ASFLAGS   := -D__ASSEMBLY__
 ###
 
 include ../Config.mk
+include ../CheckTools.mk
 
 CFLAGS    += $(foreach bool,$(COMPILE_CONFIG_BOOLS), \
              $(if $(findstring 1,$($(bool))),-D__$(bool)__=$(strip $($(bool)))))
