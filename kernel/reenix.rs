214f3c7786393b437775f8cf331b0946221ae4e2
Alexander Light <allight@cs.brown.edu>
2014-09-24 17:01:33 -0400

    make reenix.rs set the right name

--- kernel/reenix.rs
+++ kernel/reenix.rs
@@ -1,32 +1,62 @@
-#![crate_name = "main"]
-#![crate_type = "lib"]
+#![crate_name = "reenix"]
+#![crate_type = "staticlib"]
 
 #![no_std]
-#![no_split_stack]
 
 #![feature(asm, macro_rules, default_type_params, phase, globs, lang_items, intrinsics)]
 
 // The plugin phase imports compiler plugins, including regular macros.
 
-extern crate core;
 
+#[phase(plugin, link)] extern crate core;
+extern crate mm;
+extern crate alloc;
+#[phase(plugin, link)] extern crate base;
+extern crate rlibc;
+
+use base::debug::printing::DBG_WRITER;
 pub mod main;
+//mod hacky;
 
+
+#[cold]
 #[no_mangle]
 #[no_split_stack]
 pub extern "C" fn __morestack() {
-    ()
+    panic!("__morestack called. This cannot happen");
 }
 
-#[lang="eh_personality"]
-fn eh_personality() {
-    ()
-}
-#[lang="stack_exhausted"]
-fn stack_exhausted() {
-    ()
-}
+#[cold]
+#[no_mangle]
+#[inline(never)]
 #[lang="begin_unwind"]
-fn begin_unwind() {
-    ()
+#[allow(unused_must_use)]
+pub extern fn rust_begin_unwind(msg: &core::fmt::Arguments,
+                                file: &'static str,
+                                line: uint) -> ! {
+    unsafe { core::fmt::write(&mut DBG_WRITER, msg); }
+    panic!("Begin Unwind at {:s}:{:u}",file, line);
+}
+
+#[cold]
+#[inline(never)]
+#[lang="eh_personality"]
+#[allow(unused_must_use)]
+pub extern fn eh_personality() {
+    panic!("eh_personality called");
+}
+
+#[cold]
+#[inline(never)]
+#[lang = "stack_exhausted"]
+#[allow(unused_must_use)]
+pub extern fn stack_exhausted(fmt: &core::fmt::Arguments,
+                          file: &'static str,
+                          line: uint) -> ! {
+    unsafe { core::fmt::write(&mut DBG_WRITER, fmt); }
+    panic!("Stack Exhausted at {:s}:{:u}",file, line);
+}
+
+mod std {
+    pub use core::fmt;
 }
