8c57a6b960fb30fbcb234f94cc4669d6605216ba
Alexander Light <allight@cs.brown.edu>
2014-09-24 17:08:19 -0400

    make main call all init routines

--- kernel/main/mod.rs
+++ kernel/main/mod.rs
@@ -1,6 +1,9 @@
 
-use core::option::*;
+#[phase(plugin, link)] extern crate base;
+extern crate libc;
+
 use core::iter::*;
+
 
 #[no_split_stack]
 fn clear_screen(background: u16) {
@@ -11,9 +14,38 @@ fn clear_screen(background: u16) {
     }
 }
 
+extern "C" {
+    fn dbg_init();
+    fn page_init();
+    fn pt_init();
+    fn slab_init();
+    // TODO I don't think I need this yet, or ever.
+    // fn pframe_init();
+    fn acpi_init();
+    fn apic_init();
+    fn gdt_init();
+}
+
+unsafe fn run_c_init() {
+    dbg_init();
+    page_init();
+    pt_init();
+    slab_init();
+    acpi_init();
+    apic_init();
+    gdt_init();
+}
+
+fn run_rust_init() {
+    use mm;
+    mm::alloc::init();
+}
+
 #[no_mangle]
 #[no_split_stack]
 pub extern "C" fn kmain2() {
+    unsafe { run_c_init(); }
+    dbg!(debug::MM, "hi {}", "debugging");
     clear_screen(13);
     loop {}
 }
