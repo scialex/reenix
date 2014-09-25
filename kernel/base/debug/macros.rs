2a4eb29b6e2dc952c7a73348aa860c965eebf7c8
Alexander Light <allight@cs.brown.edu>
2014-09-25 14:33:45 -0400

    remove unnessecary use statement

--- kernel/base/debug/macros.rs
+++ kernel/base/debug/macros.rs
@@ -36,7 +36,6 @@ macro_rules! dbger(
 macro_rules! dbg(
     ($d:expr, $fmt:expr, $($a:expr),*) => ({
         use base::debug;
-        use core::fmt::Show;
         if (debug::dbg_active & ($d)) != debug::NONE {
             dbg_write!("{}{}-{}:{:u} : ", $d.get_color(), $d, file!(), line!());
             dbg_write!($fmt, $($a),*);
