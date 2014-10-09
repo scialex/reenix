42b8ee1ed65d21b4e52dc937a0f539d8de3487b1
Alexander Light <allight@cs.brown.edu>
2014-10-09 13:23:51 -0400

    quiet some warnings in C code

--- Global.mk
+++ Global.mk
@@ -7,7 +7,7 @@ RUST      ?= rustc
 
 RSFLAGS   += -g --target=i686-unknown-linux-gnu -Z no-landing-pads
 CFLAGS    += -fno-builtin -nostdinc -std=c99 -g3 -gdwarf-3 -fno-stack-protector -m32 -march=i686 -fsigned-char -Iinclude
-CFLAGS    += -Wall -Wredundant-decls -Wundef -Wpointer-arith -Wfloat-equal -Wnested-externs -Wvla -Winline -Wextra -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable
+CFLAGS    += -Wall -Wredundant-decls -Wundef -Wpointer-arith -Wfloat-equal -Wnested-externs -Wvla -Winline -Wextra -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable -Wno-attributes
 ASFLAGS   := -D__ASSEMBLY__
 
 ###
