64d08faddac8f557afa7360467bac8d3d73a1e4d
Alexander Light <allight@cs.brown.edu>
2014-11-18 17:23:48 -0500

    some more makefile changes

--- Global.mk
+++ Global.mk
@@ -1,3 +1,4 @@
+SHELL     := /bin/sh
 CC        := gcc
 LD        := ld
 AR        := ar
@@ -11,6 +12,8 @@ RSFLAGS   += -g -Z no-landing-pads
 CFLAGS    += -fno-builtin -nostdinc -std=c99 -g3 -fno-stack-protector -m32 -march=i686 -fsigned-char -Iinclude
 CFLAGS    += -Wall -Wredundant-decls -Wundef -Wpointer-arith -Wfloat-equal -Wnested-externs -Wvla -Winline -Wextra -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable -Wno-attributes
 ASFLAGS   := -D__ASSEMBLY__
+
+.SUFFIXES:
 
 ###
 
--- kernel/mk/Funcs.mk
+++ kernel/mk/Funcs.mk
@@ -3,13 +3,13 @@
 # Get an doc name from the crate name
 # $(1) is the name of the crate
 define doc-name
-docs/$(1)/index.html
+$(DOC_DIR)/$(1)/index.html
 endef
 
 # $(1) is the name of the crate
 # $(2) is the directory it is in.
 define set-base-crate-name
-$(eval $(1)_CRATE := libs/$(shell $(RUST) --print-file-name $(2)/lib.rs))
+$(eval $(1)_CRATE := $(BUILD_DIR)/libs/$(shell $(RUST) --print-file-name $(2)/lib.rs))
 endef
 
 define set-builtin-crate-name
@@ -30,15 +30,39 @@ define crate-name
 $($(1)_CRATE)
 endef
 
+# a rule that copies a file.
+# $(1) is the source
+# $(2) is the destination
+define copy-rule
+$(2) : $(1)
+	@ echo "[CP  ] Copying \"kernel/$$@\"..."
+	$$(HIDE_SIGIL) mkdir -p $$(dir $$@)
+	$$(HIDE_SIGIL) cp $$< $$@
+endef
+
+# invoke the linker
+# $(1) is the file to link
+# $(2) is the list of all files to give the linker
+# $(3) is the ld to give the linker
+# $(4) is the extra ldflags to pass
+define ld-rule
+$(1) : $(2) $(3) | $$(dir $(1))
+	@ echo "[LD  ] Linking for \"kernel/$$@\"..."
+ifeq ("",$(3))
+	$$(HIDE_SIGIL) $$(LD) $$(LDFLAGS) $(4) $(2) -o $$@
+else
+	$$(HIDE_SIGIL) $$(LD) -T $(3) $$(LDFLAGS) $(4) $(2) -o $$@
+endif
+endef
+
 # Get rules for external targets.
 # $(1) is the name of the target under external
 # $(2) is the name of the target we wish to build in external.
-# $(3) is the name of the file we wish to take into libs
+# $(3) is the name of the file we wish to take into $(BUILD_DIR)
 # $(4) are any flags we wish to pass down.
 define external-targets
-./libs/$(notdir $(3)) : external/$(1)/$(3)
-	@ echo "[CP  ] Copying \"kernel/$$@\"..."
-	$$(HIDE_SIGIL) cp external/$(1)/$(3) $$@
+
+$(call copy-rule,external/$(1)/$(3),$$(BUILD_DIR)/external/$(notdir $(3)))
 
 ./external/$(1)/$(3) : $$(shell find ./external/$(1) -type f -not -path ./external/$(1)/$(3))
 	@ echo "[MAKE] Recursive make of \"kernel/$$@\"..."
@@ -46,7 +70,7 @@ define external-targets
 
 .PHONEY:
 clean-$(1):
-	$$(HIDE_SIGIL) rm -f libs/$(notdir $(3)) 2>/dev/null
+	$$(HIDE_SIGIL) rm -f $$(BUILD_DIR)/external/$(notdir $(3)) 2>/dev/null
 	$$(HIDE_SIGIL) $$(MAKE) $$(MFLAGS) $$(SILENT_FLAG) -C external/$(1) clean $(4)
 endef
 
@@ -57,13 +81,16 @@ endef
 # $(4) is any additional rust flags you want.
 # $(5) is any additional rustdoc flags you want.
 define base-crate-rule
-$(call crate-name,$(2)) : $$(shell find $(1) -type f -name "*.rs") $$(foreach l,$(3), $$(call crate-name,$$(l)))
+$(call crate-name,$(2)) : $$(shell find $(1) -type f -name "*.rs") \
+                          $$(foreach l,$(3), $$(call crate-name,$$(l))) \
+                        | $$(dir $$(call crate-name,$(2)))
 	@ echo "[RS  ] Compiling \"kernel/$(1)/lib.rs\"..." # for \"kernel/$$@\""
-	$$(HIDE_SIGIL) $$(RUST) $$(RSFLAGS) $(4) $(1)/lib.rs --out-dir libs
+	$$(HIDE_SIGIL) $$(RUST) $$(RSFLAGS) $(4) $(1)/lib.rs --out-dir $$(BUILD_DIR)/libs
 
-$(call doc-name,$(2)) : $$(shell find $(1) -type f -name "*.rs") $$(foreach l,$(3), $$(call crate-name,$$(l)))
+$(call doc-name,$(2)) : $$(shell find $(1) -type f -name "*.rs") \
+						$$(foreach l,$(3), $$(call crate-name,$$(l)))
 	@ echo "[RDOC] Documenting \"kernel/$(1)\"..."
-	$$(HIDE_SIGIL) $$(RUSTDOC) $$(RDFLAGS) $(5) --output docs $(1)/lib.rs
+	$$(HIDE_SIGIL) $$(RUSTDOC) $$(RDFLAGS) $(5) --output $(DOC_DIR) $(1)/lib.rs
 
 endef
 
@@ -94,30 +121,5 @@ endef
 # $(2) is the list of dependencies.
 define plugin-rule
 $(eval $(call base-crate-rule,plugins/$(strip $(1)),$(strip $(1)),$(2),,,))
-endef
-
-# a rule that copies a file.
-# $(1) is the source
-# $(2) is the destination
-define copy-rule
-$(2) : $(1)
-	@ echo "[CP  ] Copying \"kernel/$$@\"..."
-	$$(HIDE_SIGIL) mkdir -p $$(dir $$@)
-	$$(HIDE_SIGIL) cp $$< $$@
-endef
-
-# invoke the linker
-# $(1) is the file to link
-# $(2) is the list of all files to give the linker
-# $(3) is the ld to give the linker
-# $(4) is the extra ldflags to pass
-define ld-rule
-$(1) : $(2) $(3)
-	@ echo "[LD  ] Linking for \"kernel/$$@\"..."
-ifeq ("",$(3))
-	$$(HIDE_SIGIL) $$(LD) $$(LDFLAGS) $(4) $(2) -o $$@
-else
-	$$(HIDE_SIGIL) $$(LD) -T $(3) $$(LDFLAGS) $(4) $(2) -o $$@
-endif
 endef
 
