2d190ae3a2f84f452bce2b12b9324dab810c99ee
Alexander Light <allight@cs.brown.edu>
2014-12-17 18:20:57 -0500

    stuff with todo

--- kernel/mk/Helpers.mk
+++ kernel/mk/Helpers.mk
@@ -24,52 +24,71 @@ nyi:
 todo:
 	@ echo "  Not yet done:"
 	@ echo
-	@ git grep --heading --break -Epnh "((\/\/|\/?\*) TODO( [^C][^o][^p][^y][^r][^i][^g][^h][^t]|.{1,9}$$)|^ *\*t )" \
-| sed -E "s/^\t?[0-9]+=/  /" \
-| sed -E "/^  .*;$$/d" \
-| awk '                                                       \
-    BEGIN {                                                   \
-        FS       = ":";                                       \
-        count    = 0;                                         \
-        cur_file = "";                                        \
-        prev     = "";                                        \
-        prev_cnt = 0;                                         \
-    }                                                         \
-    {                                                         \
-        if ($$0 !~ /^[0-9]+:/) {                              \
-            if ($$0 !~ /^ +/) {                               \
-                if (cur_file != "") {                         \
-                    plural = "";                              \
-                    if (prev_cnt != 1) {                      \
-                        plural = "s";                         \
-                    }                                         \
-                    printf("%s: %d thing%s to do\n%s\n",      \
-                           cur_file, prev_cnt, plural, prev); \
-                }                                             \
-                cur_file = $$0;                               \
-                prev_cnt = 0;                                 \
-                prev     = "";                                \
-            } else {                                          \
-                prev = prev "" $$0 "\n";                      \
-            }                                                 \
-        } else {                                              \
-            count    += 1;                                    \
-            prev_cnt += 1;                                    \
-            first     = $$1;                                  \
-            $$1       = "";                                   \
-            prev      = prev "" sprintf("%7s: %s\n",          \
-                                        first, $$0);          \
-        }                                                     \
-    }                                                         \
-    END {                                                     \
-        if (cur_file != "") {                                 \
-            plural = "";                                      \
-            if (prev_cnt != 1) {                              \
-                plural = "s";                                 \
-            }                                                 \
-            printf("%s: %d thing%s to do\n%s",                \
-                   cur_file, prev_cnt, plural, prev);         \
-        }                                                     \
-        printf("\nTotal of %d TODOs\n", count);               \
-    }                                                         \
+	@ git grep --heading -C 3 --break -Epnh "((\/\/|\/?\*) TODO( [^C][^o][^p][^y][^r][^i][^g][^h][^t]|.{1,9}$$)|^ *\*t )" \
+| sed -E "s/^\t?[0-9]+=/  /"                                        \
+| sed -E "s/^(\t?[0-9]+)-/\1:/"                                     \
+| sed -E "/^  .*;$$/d"                                              \
+| awk '                                                             \
+    BEGIN {                                                         \
+        FS       = ":";                                             \
+        count    = 0;                                               \
+        cur_file = "";                                              \
+        prev     = "";                                              \
+        prev_cnt = 0;                                               \
+        new_section = 0;                                            \
+    }                                                               \
+    {                                                               \
+        if ($$0 == "--") {                                          \
+            if (new_section == 0) {                                 \
+                prev = prev "" "\n"                                 \
+            }                                                       \
+        } else {                                                    \
+            if ($$0 !~ /(TODO|\*t)/) {                              \
+                if ($$0 !~ /^[0-9]+:/) {                            \
+                    if ($$0 !~ /^ +/) {                             \
+                        if (cur_file != "") {                       \
+                            plural = "";                            \
+                            if (prev_cnt != 1) {                    \
+                                plural = "s";                       \
+                            }                                       \
+                            printf("%s: %d thing%s to do\n%s\n",    \
+                                cur_file, prev_cnt, plural, prev);  \
+                        }                                           \
+                        cur_file = $$0;                             \
+                        prev_cnt = 0;                               \
+                        prev     = "";                              \
+                        new_section = 1;                            \
+                    } else {                                        \
+                        prev = prev "" $$0 "\n";                    \
+                        new_section = 1;                            \
+                    }                                               \
+                } else {                                            \
+                    new_section = 0;                                \
+                    first     = $$1;                                \
+                    $$1       = "";                                 \
+                    prev = prev "" sprintf("%7s- %s\n",             \
+                                           first, $$0);             \
+                }                                                   \
+            } else {                                                \
+                count    += 1;                                      \
+                prev_cnt += 1;                                      \
+                first     = $$1;                                    \
+                $$1       = "";                                     \
+                prev      = prev "" sprintf("%7s: %s\n",            \
+                                            first, $$0);            \
+                new_section = 0;                                    \
+            }                                                       \
+        }                                                           \
+    }                                                               \
+    END {                                                           \
+        if (cur_file != "") {                                       \
+            plural = "";                                            \
+            if (prev_cnt != 1) {                                    \
+                plural = "s";                                       \
+            }                                                       \
+            printf("%s: %d thing%s to do\n%s",                      \
+                   cur_file, prev_cnt, plural, prev);               \
+        }                                                           \
+        printf("\nTotal of %d TODOs\n", count);                     \
+    }                                                               \
     '
